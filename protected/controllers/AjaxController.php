b2e2c9a0995bc1b21268c48b3d98ec897d527916
Brilenko Sergey <sbrilenko@cayugasoft.com>
2014-05-09 11:30:03 +0300

    email

--- protected/controllers/AjaxController.php
+++ protected/controllers/AjaxController.php
@@ -25,32 +25,10 @@ class AjaxController extends CController {
         ';
 
         $headers  = "Content-type: text/html; charset=windows-1251 \r\n";
-        $headers .= "From: Cayuga Mobile <cayugamobile.com>\r\n";
-        $headers .= "Bcc: cayugamobile.com\r\n";
+        $headers .= "From: Cayuga Mobile <test@ukr.net>\r\n";
+        $headers .= "Bcc: test@ukr.net\r\n";
 
         if(mail($to, $subject, $message, $headers))
-        {
-            echo "Mail send";
-        }
-        else
-        {
-            echo "Error sanding mail";
-        }
-        die();
-        $mail = new YiiMailer();
-        $mail->setFrom('Cayuga Mobile', 'Cayuga Mobile');
-        //$mail->setTo(array("mike@cayugasoft.com", "eugene@cayugasoft.com", "katerina@cayugasoft.com"));
-        $mail->setTo("secret007@ukr.net");
-        $mail->setSubject('New contact request from Cayugamobile.com');
-        $mail->setBody('New contact request from Cayugamobile.com<br />
-                        <br />
-                        Name:    '.$yourname.'<br />
-                        Email:   '.$youremail.'<br />
-                        Message: '.$yourmessage.'<br />
-                        <br />
-                        Cayuga Mobile
-        ');
-        if($mail->send())
         {
             echo "Mail send";
         }
