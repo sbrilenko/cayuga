7bacccd0d0a4c6ca186c3c5db32cb847b8d05cf7
Brilenko Sergey <sbrilenko@cayugasoft.com>
2014-06-05 15:34:37 +0300

    google analistic

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -757,5 +757,15 @@
     /*}*/
 </style>
 <div id='dialog'></div>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-51674352-1', 'cayugamobile.com');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
